65226e6b4cf0d5a4373844084e7cf89c0e7440af
neojarvis <techadmin@iamneo.ai>
2025-03-10 05:42:43 +0000

    Auto-save-commit-10/03/2025--11:12:36

--- angularapp/src/app/app.routes.ts
+++ angularapp/src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Routes } from '@angular/router';
-import { SongListComponent } from './song-list/song-list.component';
-import { AddSongComponent } from './add-song/add-song.component';
-import { EditSongComponent } from './edit-song/edit-song.component';
+// import { Routes } from '@angular/router';
+// import { SongListComponent } from './song-list/song-list.component';
+// import { AddSongComponent } from './add-song/add-song.component';
+// import { EditSongComponent } from './edit-song/edit-song.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/songs', pathMatch: 'full' },
-  { path: 'songs', component: SongListComponent },
-  { path: 'add-song', component: AddSongComponent },
-  { path: 'edit-song/:id', component: EditSongComponent },
-  { path: 'update-song/:id', redirectTo: 'edit-song/:id', pathMatch: 'full' }
-];
+// export const routes: Routes = [
+//   { path: '', redirectTo: '/songs', pathMatch: 'full' },
+//   { path: 'songs', component: SongListComponent },
+//   { path: 'add-song', component: AddSongComponent },
+//   { path: 'edit-song/:id', component: EditSongComponent },
+//   { path: 'update-song/:id', redirectTo: 'edit-song/:id', pathMatch: 'full' }
+// ];
--- angularapp/src/main.ts
+++ angularapp/src/main.ts
@@ -4,12 +4,12 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+// import { routes } from './app/app-routing.module'
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
     provideHttpClient(),
-    provideRouter(routes)
+    // provideRouter(routes)
   ]
 }).catch(err => console.error(err));
